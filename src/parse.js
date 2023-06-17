e4ad3fcfa2629a72e8592026a373c6e62c3119ee
Matt Riley <m@ttriley.dev>
2023-06-17 21:05:31 +1000

    Unspecified changes

--- src/parse.js
+++ src/parse.js
@@ -4,7 +4,7 @@ module.exports = (f, opts = {}) => {
 
     opts.pick = [opts.pick ?? []].flat(); // coerce `pick` into an array 
 
-    let { initial = {}, select = null, pick = [], separator = '=', cache = {}, ...config } = opts;
+    let { initial = {}, select = null, pick = [], pathSep = '/', separator = '=', cache = {}, ...config } = opts;
 
     const parseKeyValuePairs = str => {
         if (!str.includes(separator)) return {};
@@ -50,41 +50,15 @@ module.exports = (f, opts = {}) => {
 
 
 
-    const parseArr = arr => arr.filter(Boolean).map(parseKeyValuePairs);
-
-
-
-    const targetPath = f;
-    // const targetPath = path.join(f.reldirpath, f.name); // first dot to allow many
-    // need to reconsider how to do this
-
     const getMasterObj = () => {
-        if (cache[targetPath]) {
-            console.warn('cache hit');
-            return cache[targetPath];
-        }
-
-        const arr = targetPath.split('/');
-
-        const entries = parseArr(arr);
-
-
-
-
-        const masterObj = _.merge({}, ...entries); // ??
-        // console.warn(masterObj);
-
-        cache[targetPath] = masterObj;
-        return cache[targetPath];
+        if (cache[f]) return cache[f];
+        const arr = f.split(pathSep);
+        const entries = arr.filter(Boolean).map(parseKeyValuePairs);
+        cache[f] = _.merge({}, ...entries);
+        return cache[f];
     };
 
-
     const masterObj = getMasterObj();
-
-    // console.warn(masterObj); // ok
-
-
-    // let selected = select ? _.get(masterObj, select) : { ...masterObj };
 
     let selected = select ? _.get(masterObj, select) : { ...masterObj };
 
@@ -127,24 +101,7 @@ module.exports = (f, opts = {}) => {
         return selected ? selected : config.default;
     }
 
-    if (select === 'event') {
-        if (selected.subject && !selected.subjects) {
-            selected.subjects = [selected.subject];
-        }
-
-        if (selected.subject1) {
-            const subjects = _.compact([selected.subject1, selected.subject2]);
-            selected.subjects = subjects;
-        }
-
-        delete selected.subject;
-        delete selected.subject1;
-        delete selected.subject2;
-    }
-
-
     // if an object:
-
 
     const transformValue = v => {
         // must be undefined (null prob ok)
