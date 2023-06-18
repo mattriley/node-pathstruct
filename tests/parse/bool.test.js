27b976424f256f430513c1f89b48feb94f4c09fe
Matt Riley <m@ttriley.dev>
2023-06-18 21:58:30 +1000

    Unspecified changes

--- tests/parse/bool.test.js
+++ tests/parse/bool.test.js
@@ -2,28 +2,28 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const parse = require('../../src/parse');
 
-test('unquoted literal true is converted to boolean true', () => {
+test('boolean true', () => {
     assert.equal(parse('foo=true').foo, true);
     assert.equal(parse('foo=[true]').foo[0], true);
     assert.equal(parse('x.foo=true').x.foo, true);
     assert.equal(parse('x.foo=[true]').x.foo[0], true);
 });
 
-test('quoted literal true remains string', () => {
+test('string true', () => {
     assert.equal(parse('foo="true"').foo, 'true');
     assert.equal(parse('foo=["true"]').foo[0], 'true');
     assert.equal(parse('x.foo="true"').x.foo, 'true');
     assert.equal(parse('x.foo=["true"]').x.foo[0], 'true');
 });
 
-test('unquoted literal false is converted to boolean false', () => {
+test('boolean false', () => {
     assert.equal(parse('foo=false').foo, false);
     assert.equal(parse('foo=[false]').foo[0], false);
     assert.equal(parse('x.foo=false').x.foo, false);
     assert.equal(parse('x.foo=[false]').x.foo[0], false);
 });
 
-test('unquoted literal false remains string', () => {
+test('string false', () => {
     assert.equal(parse('foo="false"').foo, 'false');
     assert.equal(parse('foo=["false"]').foo[0], 'false');
     assert.equal(parse('x.foo="false"').x.foo, 'false');
