7c72d9c7d4f255a55aae3cb268fdfc4000e3bd7a
Matt Riley <m@ttriley.dev>
2023-06-18 21:49:54 +1000

    Unspecified changes

--- tests/parse/bool.test.js
+++ tests/parse/bool.test.js
@@ -2,13 +2,23 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const parse = require('../../src/parse');
 
-test('string value is literal true', () => {
+test('unquoted literal true is converted to boolean true', () => {
     const struct = parse('foo=true', { select: 'foo' });
     assert.equal(struct, true);
 });
 
-test('string value is literal false', () => {
-    // this only works when using select
+test('quoted literal true remains string', () => {
+    const struct = parse('foo="true"', { select: 'foo' });
+    assert.equal(struct, 'true');
+});
+
+
+test('unquoted literal false is converted to boolean false', () => {
     const struct = parse('foo=false', { select: 'foo' });
     assert.equal(struct, false);
+});
+
+test('unquoted literal false remains string', () => {
+    const struct = parse('foo="false"', { select: 'foo' });
+    assert.equal(struct, 'false');
 });
