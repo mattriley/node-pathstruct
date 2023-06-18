b76e8d9b92b93f7c0a9d9986fca07334d7cedca8
Matt Riley <m@ttriley.dev>
2023-06-18 11:43:13 +1000

    Unspecified changes

--- tests/parse/select.test.js
+++ tests/parse/select.test.js
@@ -15,6 +15,5 @@ test('selecting an object', () => {
 // test('parsing an object into existing object', () => {
 //     const initial = { x: { foo: 'bar' } };
 //     const struct = parse('x.baz=qux', { initial, select: 'x' });
-//     // console.warn(struct);
 //     assert.deepEqual(struct, { foo: 'bar', baz: 'qux' });
 // });
