8c3292a91d65e55663e8f221c6852cf6b388fb1f
Matt Riley <m@ttriley.dev>
2023-06-18 21:51:20 +1000

    Unspecified changes

--- tests/parse/bool.test.js
+++ tests/parse/bool.test.js
@@ -3,22 +3,27 @@ const assert = require('node:assert/strict');
 const parse = require('../../src/parse');
 
 test('unquoted literal true is converted to boolean true', () => {
-    const struct = parse('foo=true', { select: 'foo' });
-    assert.equal(struct, true);
+    const struct = parse('foo=true');
+    assert.equal(struct.foo, true);
 });
 
 test('quoted literal true remains string', () => {
-    const struct = parse('foo="true"', { select: 'foo' });
-    assert.equal(struct, 'true');
+    const struct = parse('foo="true"');
+    assert.equal(struct.foo, 'true');
 });
+
+// test('unquoted literal true is converted to boolean true within object', () => {
+//     const struct = parse('x.foo=true', { select: 'foo' });
+//     assert.equal(struct, true);
+// });
 
 
 test('unquoted literal false is converted to boolean false', () => {
-    const struct = parse('foo=false', { select: 'foo' });
-    assert.equal(struct, false);
+    const struct = parse('foo=false');
+    assert.equal(struct.foo, false);
 });
 
 test('unquoted literal false remains string', () => {
-    const struct = parse('foo="false"', { select: 'foo' });
-    assert.equal(struct, 'false');
+    const struct = parse('foo="false"');
+    assert.equal(struct.foo, 'false');
 });
