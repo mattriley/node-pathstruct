6d1944e72b61f4360b78fac17dc3ca4d3027fead
Matt Riley <m@ttriley.dev>
2023-06-24 18:01:36 +1000

    Unspecified changes

--- src/modules/writer/<< write.js
+++ src/modules/writer/<< write.js
@@ -4,39 +4,29 @@ module.exports = ({ util, config }) => (obj, opts = {}) => {
 
     const keys = opts.pick.length ? opts.pick : Object.keys(flat(obj, { safe: true }));
 
-    return keys
-        .filter(k => _.has(obj, k))
-        .flatMap(k => {
-            const val = _.get(obj, k);
+    return keys.flatMap(key => {
+        const val = _.get(obj, key);
+        if (util.isEmpty(val)) return [];
 
-            if (util.isEmpty(val)) return [];
+        const processArray = val => {
+            const results = val.flatMap(val => {
+                val = processValue(val);
+                if (util.isEmpty(val)) return [];
+                return val;
+            });
 
-            const processArray = () => {
-                return val.flatMap(val => {
-                    val = processValue(val);
-                    if (util.isEmpty(val)) return [];
-                    return val;
-                });
-            };
+            return '[' + results.join(',') + ']';
+        };
 
-            const processValue = val => {
-                if (Array.isArray(val)) return processArray(val);
-                const str = val.toString().replace('/', '_').trim();
-                const shouldQuote = str.includes(' ') || val === 'true' || val === 'false';
-                return shouldQuote ? `"${str}"` : str;
-            };
+        const processValue = val => {
+            if (Array.isArray(val)) return processArray(val);
+            const str = val.toString().replace('/', '_').trim();
+            const shouldQuote = str.includes(' ') || val === 'true' || val === 'false';
+            return shouldQuote ? `"${str}"` : str;
+        };
 
+        return [key, processValue(val)].join(config.keyValueSeparator);
 
-
-            const processed = processValue(val);
-
-            const strVal = Array.isArray(processed) ? '[' + processed.join(',') + ']' : processed;
-
-            const res = [k, strVal].join(config.keyValueSeparator);
-
-            return res;
-
-
-        }).join(' ');
+    }).join(' ');
 };
 
