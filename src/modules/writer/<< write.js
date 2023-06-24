ecdc1a6bf3bf4e37aa0ebf5fee4b2d3542dbfa38
Matt Riley <m@ttriley.dev>
2023-06-24 18:10:38 +1000

    Unspecified changes

--- src/modules/writer/<< write.js
+++ src/modules/writer/<< write.js
@@ -7,17 +7,8 @@ module.exports = ({ self, util, config }) => (obj, opts = {}) => {
     return keys.flatMap(key => {
         const val = _.get(obj, key);
         if (util.isEmpty(val)) return [];
-
-        const stringifyValue = val => {
-            if (util.isEmpty(val)) return undefined;
-            if (Array.isArray(val)) return self.stringifyArray(val);
-            const str = val.toString().replace('/', '_').trim();
-            const shouldQuote = str.includes(' ') || val === 'true' || val === 'false';
-            return shouldQuote ? `"${str}"` : str;
-        };
-
-        return [key, stringifyValue(val)].join(config.keyValueSeparator);
-
+        return [key, self.stringifyValue(val)].join(config.keyValueSeparator);
     }).join(' ');
+
 };
 
