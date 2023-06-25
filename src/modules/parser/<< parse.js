434c96749b7e65929f42d8ce006a68dca7cb0e67
Matt Riley <m@ttriley.dev>
2023-06-25 10:13:36 +1000

    Unspecified changes

--- src/modules/parser/<< parse.js
+++ src/modules/parser/<< parse.js
@@ -3,15 +3,18 @@ module.exports = ({ self }) => (path, opts = {}) => {
     const { valid, errors } = self.validate(opts);
     if (!valid) throw (errors);
 
-    const original = opts.cache[path] ?? (opts.cache[path] = self.invokeParsers(path));
-    const selected = opts.select ? _.get(original, opts.select, {}) : original;
-    const defaulted = _.isPlainObject(selected) ? _.defaultsDeep(selected, opts.initial) : selected;
-
-    if (opts.pick.length && !_.isPlainObject(defaulted)) {
-        throw new Error('Failed to pick; target is not a plain object');
-    }
-
-    const picked = opts.pick.length ? _.pick(defaulted, opts.pick) : defaulted;
-    return self.transformValue(picked);
+    return _.flow([
+        () => opts.cache[path] ?? (opts.cache[path] = self.invokeParsers(path)),
+        obj => opts.select ? _.get(obj, opts.select, {}) : obj,
+        obj => _.isPlainObject(obj) ? _.defaultsDeep(obj, opts.initial) : obj,
+        obj => {
+            if (opts.pick.length && !_.isPlainObject(obj)) {
+                throw new Error('Failed to pick; target is not a plain object');
+            }
+            return obj;
+        },
+        obj => opts.pick.length ? _.pick(obj, opts.pick) : obj,
+        obj => self.transformValue(obj)
+    ])();
 
 };
