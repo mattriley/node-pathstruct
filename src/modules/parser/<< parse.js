116e1c41c3236e48f712e5984325aedf713b6b09
Matt Riley <m@ttriley.dev>
2023-06-25 10:18:13 +1000

    Unspecified changes

--- src/modules/parser/<< parse.js
+++ src/modules/parser/<< parse.js
@@ -4,17 +4,15 @@ module.exports = ({ self }) => (path, opts = {}) => {
     if (!valid) throw (errors);
 
     return _.flow([
-        () => opts.cache[path] ?? (opts.cache[path] = self.invokeParsers(path)),
+        obj => obj ?? (opts.cache[path] = self.invokeParsers(path)),
         obj => opts.select ? _.get(obj, opts.select, {}) : obj,
         obj => _.isPlainObject(obj) ? _.defaultsDeep(obj, opts.initial) : obj,
         obj => {
-            if (opts.pick.length && !_.isPlainObject(obj)) {
-                throw new Error('Failed to pick; target is not a plain object');
-            }
-            return obj;
+            if (!opts.pick.length || _.isPlainObject(obj)) return obj;
+            throw new Error('Failed to pick; target is not a plain object');
         },
         obj => opts.pick.length ? _.pick(obj, opts.pick) : obj,
         obj => self.transformValue(obj)
-    ])();
+    ])(opts.cache[path]);
 
 };
