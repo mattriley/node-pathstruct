2ffc6ff49566083074be325e07752f0c07c7c227
Matt Riley <m@ttriley.dev>
2024-05-07 21:38:51 +1000

    Unspecified changes

--- tests/util/apply.test.js
+++ tests/util/apply.test.js
@@ -2,8 +2,8 @@ module.exports = ({ test, assert }) => ({ array }) => {
 
     test('apply keys', () => {
         const obj = { sub: ['foo', 'bar'], 'sub+': ['baz'], 'sub-': ['foo'] };
-        const actual = array.apply(obj, ['sub']);
-        const expected = { sub: ['bar', 'baz'] };
+        const actual = array.apply(obj, ['subjects', 'sub']);
+        const expected = { subjects: ['bar', 'baz'] };
         assert.deepEqual(actual, expected);
         assert.equal(actual['sub+'], undefined);
         assert.equal(actual['sub-'], undefined);
