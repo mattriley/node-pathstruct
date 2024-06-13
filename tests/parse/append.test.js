c08e95beda27cc7e268cb5b40db873df39520ff5
Matt Riley <m@ttriley.dev>
2024-06-13 22:30:08 +1000

    Unspecified changes

--- tests/parse/append.test.js
+++ tests/parse/append.test.js
@@ -12,6 +12,12 @@ module.exports = ({ test, assert }) => ({ parse }) => {
         assert.deepEqual(struct, { 'foo': ['baz'] });
     });
 
+    test('append runs last', () => {
+        const initial = {};
+        const struct = parse('foo+=[baz] foo=[bar]', { initial });
+        assert.deepEqual(struct, { 'foo': ['bar', 'baz'] });
+    });
+
     test('remove', () => {
         const initial = { foo: ['bar', 'baz'] };
         const struct = parse('foo-=[baz]', { initial });
@@ -22,6 +28,12 @@ module.exports = ({ test, assert }) => ({ parse }) => {
         const initial = {};
         const struct = parse('foo-=[baz]', { initial });
         assert.deepEqual(struct, undefined);
+    });
+
+    test('remove runs last', () => {
+        const initial = {};
+        const struct = parse('foo-=[baz] foo=[bar,baz]', { initial });
+        assert.deepEqual(struct, { 'foo': ['bar'] });
     });
 
 };
