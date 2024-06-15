5013291ea1b447f1b2bcdb70a95668204a06bb36
Matt Riley <m@ttriley.dev>
2024-06-15 12:20:35 +1000

    Unspecified changes

--- src/modules/parser/apply-operators.js
+++ src/modules/parser/apply-operators.js
@@ -7,12 +7,12 @@ module.exports = () => obj => {
 
     return Object.entries(obj).reduce((acc, [key, val]) => {
         const operator = key.slice(-1);
-        const targetKey = key.slice(0, -1);
+        const cleanKey = key.slice(0, -1);
         const operation = operations[operator];
         if (!operation) return acc;
         delete acc[key];
-        const newVal = operation(acc[targetKey] ?? [], val);
-        return { ...acc, [targetKey]: newVal };
+        const newVal = operation(acc[cleanKey] ?? [], val);
+        return { ...acc, [cleanKey]: newVal };
     }, obj);
 
 };
