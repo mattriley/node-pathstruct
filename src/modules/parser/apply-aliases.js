321c02748944aea5b1157dc7e30f06a85f23abf4
Matt Riley <m@ttriley.dev>
2024-06-15 11:17:37 +1000

    Unspecified changes

--- src/modules/parser/apply-aliases.js
+++ src/modules/parser/apply-aliases.js
@@ -1,11 +1,10 @@
 module.exports = () => (obj, aliasLookup) => {
 
-    return Object.entries(obj).reduce((acc, [key, val]) => {
-        const operator = ['+', '-'].find(op => key.endsWith(op));
+    return _.mapKeys(obj, (val, key) => {
+        const operator = ['+', '-'].find(op => key.endsWith(op)) ?? '';
         const cleanKey = operator ? key.slice(0, -1) : key;
         const mainKey = aliasLookup[cleanKey] ?? cleanKey;
-        const newKey = mainKey + (operator ?? '');
-        return { ...acc, [newKey]: val };
-    }, {});
+        return mainKey + operator;
+    });
 
 };
