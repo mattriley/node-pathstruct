90b38f21d609b49fb1709aed0bca14f0cd52b054
Matt Riley <m@ttriley.dev>
2025-06-21 10:27:03 +1000

    Unspecified changes

--- src/modules/parser/_transform-value.js
+++ src/modules/parser/_transform-value.js
@@ -1,16 +1,26 @@
+// Optimised on 21 June 2025 with help from ChatGPT.
+
 const nils = ['nil', 'null', '""', ''];
 const removeSurroundingDoubleQuotes = str => str.replace(/^"(.*)"$/, '$1');
 
+// Native plain object check (safer/faster than Lodash)
+const isPlainObject = val =>
+    val !== null &&
+    typeof val === 'object' &&
+    Object.getPrototypeOf(val) === Object.prototype;
+
 module.exports = ({ self, config }) => val => {
 
-    if (_.isPlainObject(val)) return self.transformObject(val);
+    if (isPlainObject(val)) return self.transformObject(val);
     if (Array.isArray(val)) return self.transformArray(val);
-    if (nils.some(nil => val === nil)) return undefined;
-    if (val.toString() === 'false') return false;
-    if (val.toString() === 'true') return true;
-    val = val.replace(config.pathSeparatorEncoded, config.pathSeparator);
-    return removeSurroundingDoubleQuotes(val);
+    if (nils.includes(val)) return undefined;
+
+    const str = val.toString();
+
+    if (str === 'false') return false;
+    if (str === 'true') return true;
+
+    const replaced = str.replace(config.pathSeparatorEncoded, config.pathSeparator);
+    return removeSurroundingDoubleQuotes(replaced);
 
 };
-
-
