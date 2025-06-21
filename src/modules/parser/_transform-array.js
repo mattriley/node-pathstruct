8154ff4252ba10e32c4776785db6601ba5a31743
Matt Riley <m@ttriley.dev>
2025-06-21 10:24:26 +1000

    Unspecified changes

--- src/modules/parser/_transform-array.js
+++ src/modules/parser/_transform-array.js
@@ -1,6 +1,16 @@
+// Optimised on 21 June 2025 with help from ChatGPT.
+
 module.exports = ({ self }) => arr => {
 
-    const res = arr.map(self.transformValue).filter(v => v !== undefined);
-    return res.length ? res : undefined;
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const transformed = self.transformValue(arr[i]);
+        if (transformed !== undefined) {
+            result.push(transformed);
+        }
+    }
+
+    return result.length > 0 ? result : undefined;
 
 };
