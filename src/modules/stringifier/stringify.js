047430128c483fd6b0d23f785e0b078f87162c54
Matt Riley <m@ttriley.dev>
2025-06-21 10:50:50 +1000

    Unspecified changes

--- src/modules/stringifier/stringify.js
+++ src/modules/stringifier/stringify.js
@@ -1,20 +1,29 @@
 module.exports = ({ self, config }) => (obj, options = {}) => {
 
     const { valid, errors } = self.validate({ obj, options });
-    if (!valid) throw (errors);
+    if (!valid) throw errors;
 
     const defaultOptions = { ...config, pick: Object.keys(obj) };
     const opts = { ...defaultOptions, ...options };
 
-    const target = _.pick(obj, opts.pick);
+    // Native pick equivalent
+    const target = {};
+    for (const key of opts.pick) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            target[key] = obj[key];
+        }
+    }
+
     const flatObj = $.obj.flatten(target, { delimiter: '.' });
 
     const stringify = ([key, val]) => {
-        const str = self.stringifyValue(val, opts);
         if (self.isEmpty(val)) return [];
+        const str = self.stringifyValue(val, opts);
         return [key, str].join(config.keyValueSeparator);
     };
 
-    return Object.entries(flatObj).flatMap(stringify).join(opts.keyValueDelimiter);
+    return Object.entries(flatObj)
+        .flatMap(stringify)
+        .join(opts.keyValueDelimiter);
 
 };
