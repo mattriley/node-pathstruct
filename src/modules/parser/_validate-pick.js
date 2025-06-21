2af1de985b14801caf5fac0db308b427332b45fa
Matt Riley <m@ttriley.dev>
2025-06-21 11:00:52 +1000

    Unspecified changes

--- src/modules/parser/_validate-pick.js
+++ src/modules/parser/_validate-pick.js
@@ -1,13 +1,6 @@
 module.exports = () => (obj, opts) => {
 
-    if (!opts.pick.length || (
-        obj !== null &&
-        typeof obj === 'object' &&
-        Object.getPrototypeOf(obj) === Object.prototype
-    )) {
-        return obj;
-    }
-
+    if (!opts.pick.length || $.obj.isPlain(obj)) return obj;
     throw new Error('Failed to pick; target is not a plain object');
 
 };
