594889be94d44ef8a9b85cae8ee8be59cceed60f
Matt Riley <m@ttriley.dev>
2025-06-21 15:17:23 +1000

    Unspecified changes

--- src/modules/parser/parse.js
+++ src/modules/parser/parse.js
@@ -7,8 +7,7 @@ module.exports = ({ self }) => (path, options = {}) => {
     if (!valid) throw (errors);
 
     const defaultOptions = { select: undefined, aliases: {}, pick: [], cache: {}, initial: {} };
-    // const opts = _.defaults({}, options, defaultOptions);
-    const opts = { ...defaultOptions, ...options }
+    const opts = _.defaults({}, options, defaultOptions);
 
     const aliasLookup = Object.fromEntries(Object.entries(opts.aliases).flatMap(([key, aliases]) => {
         return aliases.map(alias => [alias, key]);
