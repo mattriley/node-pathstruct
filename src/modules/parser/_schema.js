d7a843c265e91670b0b50ede535522d26f52b63a
Matt Riley <m@ttriley.dev>
2025-08-01 01:02:04 +1000

    Unspecified changes

--- src/modules/parser/_schema.js
+++ src/modules/parser/_schema.js
@@ -1,15 +1,36 @@
-const Ajv = require('ajv/dist/2020');
-
-module.exports = ({ self }) => {
-
-    console.warn(self.schema)
-
-    const ajv = new Ajv();
-    const validate = ajv.compile(self.schema);
-
-    return data => {
-        const valid = validate(data);
-        return { valid, errors: validate.errors };
+module.exports = () => {
+    return {
+        type: 'object',
+        required: ['path'],
+        properties: {
+            path: {
+                type: 'string'
+            },
+            options: {
+                type: 'object',
+                additionalProperties: false,
+                properties: {
+                    initial: {
+                        type: 'object'
+                    },
+                    select: {
+                        type: 'string'
+                    },
+                    pick: {
+                        type: 'array',
+                        items: {
+                            type: 'string'
+                        }
+                    },
+                    aliases: {
+                        type: 'object'
+                    },
+                    cache: {
+                        type: 'object'
+                    }
+                }
+            }
+        }
     };
 
 };
--- src/modules/parser/_validate.js
+++ src/modules/parser/_validate.js
@@ -1,43 +1,13 @@
 const Ajv = require('ajv/dist/2020');
 
-const schema = {
-    type: 'object',
-    required: ['path'],
-    properties: {
-        path: {
-            type: 'string'
-        },
-        options: {
-            type: 'object',
-            additionalProperties: false,
-            properties: {
-                initial: {
-                    type: 'object'
-                },
-                select: {
-                    type: 'string'
-                },
-                pick: {
-                    type: 'array',
-                    items: {
-                        type: 'string'
-                    }
-                },
-                aliases: {
-                    type: 'object'
-                },
-                cache: {
-                    type: 'object'
-                }
-            }
-        }
-    }
-};
+module.exports = ({ self }) => {
 
-const ajv = new Ajv();
-const validate = ajv.compile(schema);
+    const ajv = new Ajv();
+    const validate = ajv.compile(self.schema);
 
-module.exports = () => data => {
-    const valid = validate(data);
-    return { valid, errors: validate.errors };
+    return data => {
+        const valid = validate(data);
+        return { valid, errors: validate.errors };
+    };
+
 };
