65d122c439fb7e4ac3136d4ef671d9e1e01f000b
Matt Riley <m@ttriley.dev>
2026-01-13 22:42:04 +1100

    Unspecified changes

---
+++ tests/parse/parse-semicolon.test.js
@@ -0,0 +1,14 @@
+module.exports = ({ test, assert }) => ({ parse }) => {
+
+    test('parsing quoted string with spaces including semicolon', () => {
+        const struct = parse('foo="bar;bar"');
+        assert.deepEqual(struct, { foo: 'bar;bar' });
+    });
+
+    test('parsing terminated string with spaces', () => {
+        const struct = parse('foo=bar bar;');
+        assert.deepEqual(struct, { foo: 'bar bar' });
+    });
+
+
+};
--- tests/parse/parse.test.js
+++ tests/parse/parse.test.js
@@ -25,16 +25,6 @@ module.exports = ({ test, assert }) => ({ parse }) => {
         assert.deepEqual(struct, { foo: 'bar bar' });
     });
 
-    test('parsing quoted string with spaces including semicolon', () => {
-        const struct = parse('foo="bar;bar"');
-        assert.deepEqual(struct, { foo: 'bar;bar' });
-    });
-
-    test('parsing terminated string with spaces', () => {
-        const struct = parse('foo=bar bar;');
-        assert.deepEqual(struct, { foo: 'bar bar' });
-    });
-
     test('parsing an array of string', () => {
         const struct = parse('foo=[bar,baz]');
         assert.deepEqual(struct, { foo: ['bar', 'baz'] });
